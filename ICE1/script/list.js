07e448bff992bb262445417e94c780e470059048
Pragti Sharma <you@example.com>
2024-01-12 15:19:45 -0500

    compete javascript code

--- ICE1/script/list.js
+++ ICE1/script/list.js
@@ -1,31 +1,43 @@
 // ADDING ITEMS TO START AND END OF LIST
 // Get the <ul> element
+var groceryList = document.getElementById("groceries");
 
 // ADD NEW ITEM TO END OF LIST
 // Create element
+const newEndListItem = document.createElement("li");
 // Create text node
+const EndlistItemContent = document.createTextNode("New Item at End");
 // Add text node to element
+newEndListItem.appendChild(EndlistItemContent);
 // Add element end of list
+groceryList.appendChild(newEndListItem);
 
 // ADD NEW ITEM START OF LIST
 // Create element
+const newStartListItem = document.createElement("li");
 // Create text node
+const StartlistItemContent = document.createTextNode("New Item at Start");
 // Add text node to element
-// Add element to list
-
-
+newStartListItem.appendChild(StartlistItemContent);
+// Add element to start of list
+groceryList.insertBefore(newStartListItem, groceryList.firstChild);
 
 // All <li> elements
+var ListItems = document.querySelectorAll("li");
 
 // ADD A CLASS OF COOL TO ALL LIST ITEMS
-// Counter variable
 // Loop through elements
-// Change class to cool
-
+for (var i = 0; i < ListItems.length; i++) {
+    // Change class to cool
+    ListItems[i].classList.add("cool");
+}
 
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
 // h2 element
-// h2 text
+var contentSubhead = document.getElementById("contentSubhead");
 // No. of <li> elements
+var noOfItems = ListItems.length;
 // Content
+var Content = contentSubhead.innerHTML;
 // Update h2 using innerHTML (not textContent) because it contains markup
+contentSubhead.innerHTML = Content + ' <span ="numberitems">' + noOfItems + '</span>';
