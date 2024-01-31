357caf9c3983ab346b99a97bf3ca8878669ebefe
Pragti Sharma <you@example.com>
2024-01-31 04:25:23 -0500

    array file completed

---
+++ ICE3/scripts/app.js
@@ -0,0 +1,42 @@
+/*
+Pragti sharma
+January 30,2024
+ICE-3 PART 1 
+*/
+//all about arrays
+//array literal 
+let myArray=[1,2,3,4,5];
+
+//creating an array using constructor
+let myArrayConstructor= new Array();
+console.log(`Empty array: ${myArrayConstructor}`);
+myArrayConstructor,push(24);
+console.log(`Not Empty array: ${myArrayConstructor}`);
+
+
+//create array with values using constructor 
+let myConstructorArray= new Array(10,20,30,40,50);
+console.log(`#value array: ${myConstructorArray}`);
+console.log(`#value array obj `, myConstructorArray);
+ //create array using constructor
+let array2=new Array(10);
+console.log(`New array: ${array2}`);
+//add push 80 to array
+array2.push(80);
+console.log(`New array: ${array2}`);
+//Tuesday
+let TuesdayArray=new Array(10).fill("Tuesday!");
+console.log(`Class is on  ${tuesday}`);
+function square(x){
+    return x*x;
+}
+let nums=new Array(1,33,22,5);
+let squaredNums=nums.map(function(value){
+    return square
+})
+console.log(`squared values: ${squaredNums}`);
+//TODO rewrite 
+//let squaredNums=nums.map(function(value){
+   // return square(value);
+   //});
+   //using arrow functions
---
+++ ICE3/scripts/selectors.js
@@ -0,0 +1,18 @@
+/*
+Pragti sharma
+January 30,2024
+ICE-3 PART 2
+*/
+conxole.log("selector is loaded")
+$(function(){
+    console.log("DOM is ready")
+});
+function highlight(element){
+    element.css('background color',"#fcf30040")
+}
+function cssSelector(){
+    //updating by tag (<p></p>)
+    element.css('background color',"#2a9d8f")
+    //updating by class(class="red box")
+    
+}
