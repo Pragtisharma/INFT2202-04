3fd8a0c3b021d3dfbb8b3c09699673b013fcd858
Pragti Sharma <you@example.com>
2024-01-31 04:41:48 -0500

    tranversingTheDOM

--- ICE3/scripts/selectors.js
+++ ICE3/scripts/selectors.js
@@ -3,28 +3,36 @@ Pragti sharma
 January 30,2024
 ICE-3 PART 2
 */
-conxole.log("selector.js loaded")
+conxole.log("selector.js loaded");
 $(function(){
-    console.log("DOM is ready")
+    console.log("DOM is ready");
 });
 /**
  * 
  * @param {*} element 
  */
 function highlight(element){
-    element.css('background color',"#fcf30040")
+    element.css('background color',"#fcf30040");
 }
 function cssSelector(){
     //updating by tag (<p></p>)
-    $('p').css('background color',"#2a9d8f")
+    $('p').css('background color',"#2a9d8f");
     //updating by class(class="red box")
-    $('.red-box').css('background-color',"2a9d8f")
+    $('.red-box').css('background-color',"2a9d8f");
     //updating by id
-    $('#list').css('font-size','40px')
-    $('li:even').css('background-color',"#fcf30040") 
+    $('#list').css('font-size','40px');
+    $('li:even').css('background-color',"#fcf30040") ;
 }
 function tranversingTheDOM(){
-    $('#LIST').prev().css('background-color',"2a9d8f")
+    //$('#LIST').prev().css('background-color',"2a9d8f")
+    //$('#LIST').next().css('background-color',"2a9d8f")
+    //$('#LIST').prev().next().css('background-color',"2a9d8f")
+    $('#LIST').find('li').css('background-color',"2a9d8f");
+    //transverse up 1 level
+    $('#LIST').parent('div').css('font-size','24px');
+    //transverse all the way up of the body
+    $('#LIST').parents('body').css('font-size','24px');
+
 }
 //cssSelectors();
 tranversingTheDOM();
