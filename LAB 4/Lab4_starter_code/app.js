61c06ca47a11bb25170fd54dd6deeaa043828edb
Pragti Sharma <you@example.com>
2024-04-18 22:02:56 -0400

    Make an app.js file for your entry file

---
+++ LAB 4/Lab4_starter_code/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const pug = require('pug');
+const user = require('./routes/userroute.js');
+const app = express(); // Initialize the Express app
+
+// Setup template engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+app.get('/',(req,res)=>{
+    res.render('./pages/home',{
+        pageTitle:"INFT2202- USER VIEW"
+    })
+})
+
+app.use('/', user);
+
+// Listen on port 3000
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
